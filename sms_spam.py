870cf3bae9f8c15c3b576a4f10b83ec12427856d
Bret Runestad <bret.runestad@gmail.com>
2015-02-11 17:27:37 -0500

    finished initial array

--- sms_spam.py
+++ sms_spam.py
@@ -7,20 +7,26 @@ from sklearn.feature_extraction.text import TfidfVectorizer as TfidV
 def main_function(bloblist, word1, word2):
     lista = []
     for blobstring in bloblist:
-        x = findword(blobstring.lower(), word1)
-        y = findword(blobstring.lower(), word2)
+        a = findword(blobstring.lower(), 'i')
+        b = findword(blobstring.lower(), 'free')
+        c = findword(blobstring.lower(), 'mobile')
+        d = findword(blobstring.lower(), 'call')
+        e = findword(blobstring.lower(), 'cash')
+        f = findword(blobstring.lower(), 'cust')
+        g = findword(blobstring.lower(), 'cash')
+        h = findword(blobstring.lower(), 'stop')
+        i = findword(blobstring.lower(), 'reply')
+        j = findword(blobstring.lower(), 'txt')
         l1 = wordlength(blobstring, 0, 10)
         l2 = wordlength(blobstring, 10, 20)
         l3 = wordlength(blobstring, 20, 30)
         l4 = wordlength(blobstring, 30, 40)
         l5 = wordlength(blobstring, 40, 999999)
-        nc1 = numbercheck(blobstring, 1)
-        nc2 = numbercheck(blobstring, 2)
-        nc3 = numbercheck(blobstring, 3)
-        nc5 = numbercheck(blobstring, 5)
-        nc10 = numbercheck(blobstring, 10)
+        nc1 = numbercheck1(blobstring)
+        nc5 = numbercheck5(blobstring)
+        nc10 = numbercheck10(blobstring)
         z = startword(blobstring)
-        lista.append([x, y, l1, l2, l3, l4, l5, nc1, nc2, nc3, nc5, nc10, z])
+        lista.append([a, b, c, d, e, f, g, h, i, j, l1, l2, l3, l4, l5, nc1, nc5, nc10, z])
     return lista
 
 
@@ -45,6 +51,20 @@ def wordlength(blobstring, num1, num2):
 def numbercheck1(blobstring):
     astring = str(blobstring)
     if re.search(r"[0-9]{1}", astring):
+        return 1
+    else:
+        return 0
+
+def numbercheck5(blobstring):
+    astring = str(blobstring)
+    if re.search(r"[0-9]{5}", astring):
+        return 1
+    else:
+        return 0
+
+def numbercheck10(blobstring):
+    astring = str(blobstring)
+    if re.search(r"[0-9]{10}", astring):
         return 1
     else:
         return 0
