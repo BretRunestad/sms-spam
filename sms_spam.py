4327afe19443cd733692c914e7aab3223aea6ea1
Bret Runestad <bret.runestad@gmail.com>
2015-02-11 21:43:26 -0500

    added some clustering

--- sms_spam.py
+++ sms_spam.py
@@ -4,6 +4,8 @@ import re
 
 
 def word_frequency(bloblist):
+    """Creates a dictionary of all the words that appear in the list
+    and the number of times they appear"""
     a_dict = {}
     for blob in bloblist:
         xlist = [x for x in blob.words]
@@ -16,11 +18,14 @@ def word_frequency(bloblist):
 
 
 def top_dict(a_dict, num):
+    """Returns the top 'num' most frequently appearing words"""
     sorted_list = sorted(a_dict.items(), key=lambda x: x[1], reverse=True)
     return sorted_list[0:num]
 
 
 def main_function(bloblist, word1, word2):
+    """Makes a list of lists.  Each list is a list of the 0/1 values
+    for each feature"""
     lista = []
     for blobstring in bloblist:
         a = findword(blobstring.lower(), 'i')
@@ -42,11 +47,13 @@ def main_function(bloblist, word1, word2):
         nc5 = numbercheck5(blobstring)
         nc10 = numbercheck10(blobstring)
         z = startword(blobstring)
-        lista.append([a, b, c, d, e, f, g, h, i, j, l1, l2, l3, l4, l5, nc1, nc5, nc10, z])
+        lista.append([a, b, c, d, e, f, g, h, i, j, l1, l2, l3, l4, l5,
+                      nc1, nc5, nc10, z])
     return lista
 
 
 def findword(blobstring, word):
+    """If the word is in the string, returns 1.  Otherwise returns 0."""
     if blobstring.find(word) == -1:
         return 0
     else:
@@ -54,6 +61,8 @@ def findword(blobstring, word):
 
 
 def startword(blobstring):
+    """Checks to see if the string starts with word spam.  1 for spam,
+    0 for ham."""
     if blobstring.starts_with('spam'):
         return 1
     else:
@@ -61,6 +70,8 @@ def startword(blobstring):
 
 
 def wordlength(blobstring, num1, num2):
+    """Checks to see if the number of words in the string is between
+    num1 and num2"""
     if num1 < len(blobstring.words) <= num2:
         return 1
     else:
@@ -68,6 +79,7 @@ def wordlength(blobstring, num1, num2):
 
 
 def numbercheck1(blobstring):
+    """Checks to see if there's a number in the string"""
     astring = str(blobstring)
     if re.search(r"[0-9]{1}", astring):
         return 1
@@ -76,6 +88,7 @@ def numbercheck1(blobstring):
 
 
 def numbercheck5(blobstring):
+    """Checks to see if there's 5 numbers in a row in the string"""
     astring = str(blobstring)
     if re.search(r"[0-9]{5}", astring):
         return 1
@@ -84,6 +97,7 @@ def numbercheck5(blobstring):
 
 
 def numbercheck10(blobstring):
+    """Checks to see if there's 10 numbers in a row in the string"""
     astring = str(blobstring)
     if re.search(r"[0-9]{10}", astring):
         return 1
